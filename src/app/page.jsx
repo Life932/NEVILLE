6fdf8207a4a3ec05ab7fa1ee34b9a4c47e551d9a
Abdullah Ahmad Sadik <jesmin812020@gmail.com>
2026-03-20 17:15:21 +0600

    Added Impact Grid to the Homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,15 @@
 import Hero from "./ui/home/Hero";
 import Navbar from "./ui/Navbar";
 import InfoBLock from "./ui/home/InfoBlock";
+import ImpactGrid from "./ui/home/ImpactGrid";
+
 export default function Home() {
   return (
     <div className="w-full">
       {/* Hero Section at the very top */}
       <Hero />
       <InfoBLock />
+      <ImpactGrid />
 
       {/* 
         Later, we will add more sections here! Like:
