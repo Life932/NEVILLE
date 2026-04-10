a77473a6e032224e76adbff7ac023bf9f042ae83
Abdullah Ahmad Sadik <jesmin812020@gmail.com>
2026-04-10 13:55:24 +0600

    feat(page): Add new boilerplates for Pages.

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,9 @@
+import InfoSection from "../ui/home/InfoSection";
+
+export default function ContactPage() {
+  return (
+    <div className="w-full">
+      <InfoSection />
+    </div>
+  );
+}
---
+++ src/app/initiatives/page.jsx
@@ -0,0 +1,9 @@
+import InfoSection from "../ui/home/InfoSection";
+
+export default function InitiativesPage() {
+  return (
+    <div className="w-full">
+      <InfoSection />
+    </div>
+  );
+}
