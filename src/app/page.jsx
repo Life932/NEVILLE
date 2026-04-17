9f20efca6a40b4c7249c374a11680ad15704710a
Abdullah Ahmad Sadik <jesmin812020@gmail.com>
2026-04-17 18:57:04 +0600

    feat ( UI ): Add a the redesigned NavBar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import Hero2 from "./ui/home/Hero2";
 import Hero from "./ui/home/Hero";
 import Navbar from "./ui/Navbar";
 import InfoSection from "./ui/home/InfoSection";
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <div className="w-full">
       {/* Hero Section at the very top */}
+      <Hero2 />
       <Hero />
       <InfoSection />
       <ImpactGrid />
