9b1a09c6916bbc14ec8f489d4261a92a2f35793c
Life932 <jesmin812020@gmail.com>
2026-08-01 05:54:56 +0600

    feat: implement ClickSpark interactive effect and integrate into root layout

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Footer from "./ui/Footer";
 import ResizableNavbar from "@/components/ui/resizable-navbar";
 import { ThemeProvider } from "@/components/theme-provider";
+import ClickSpark from "@/components/ui/ClickSpark";
 
 export const metadata = {
   title: "Neville Youth Initiative",
@@ -13,11 +14,20 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className="font-sans antialiased flex flex-col min-h-screen relative">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ClickSpark
+            sparkColor="#4866FA"
+            sparkSize={10}
+            sparkRadius={18}
+            sparkCount={6}
+            duration={350}
+            easing="ease-out"
+          >
           <ResizableNavbar />
 
-          <main className="grow pt-20">{children}</main>
+            <main className="grow pt-20">{children}</main>
 
           <Footer />
+          </ClickSpark>
         </ThemeProvider>
       </body>
     </html>
